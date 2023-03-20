2ddc150ca2755419812f1830b896c90c69682704
tonguedes <91443577+tonguedes@users.noreply.github.com>
2023-03-20 14:02:17 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
           {
               if (!$('.navbar').hasClass('fixed'))
               {
-                  $('.navbar').stop().addClass('fixed').css('top', '-100px').animate(
+                  $('.navbar').stop().addClass('fixed').css('top', '-100px',).animate(
                       {
                           'top': '0px'
                  }, 600);
